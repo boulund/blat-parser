5e54ff30724269e604e536e85c9efb7a83ac9bf5
Fredrik Boulund <fredrik.boulund@ki.se>
2016-11-10 08:09:21 +0000

    removed imap

--- parse_blat_m.py
+++ parse_blat_m.py
@@ -2,7 +2,6 @@
 # Fredrik Boulund 2015
 # Toying with groupby to parse blat output
 
-from itertools import imap
 from string import split
 from collections import defaultdict
 
@@ -18,7 +17,7 @@ for hit in imap(split, f):
         hitlists[hit[0]].append(tuple(hit[1:]))
 
 for peptide, hitlist in hitlists.iteritems():
-    max_pid = max(imap(float, (h[1] for h in hitlist)))
+    max_pid = max(map(float, (h[1] for h in hitlist)))
     max_pid_diff = 5.00
     filtered = [h for h in hitlist if float(h[1]) >= max_pid-max_pid_diff]
     if len(filtered)>0:
