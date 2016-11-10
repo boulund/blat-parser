c92981377d16809baf3ea7a78f221e4b68db7bf0
Fredrik Boulund <fredrik.boulund@ki.se>
2016-11-10 08:13:34 +0000

    removed iteritems

--- parse_blat_m.py
+++ parse_blat_m.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.5
 # Fredrik Boulund 2015
 # Toying with groupby to parse blat output
 
@@ -15,7 +15,7 @@ for hit in map(str.split, f):
     if float(hit[2]) >= min_identity and int(hit[3]) >= min_matches:
         hitlists[hit[0]].append(tuple(hit[1:]))
 
-for peptide, hitlist in hitlists.iteritems():
+for peptide, hitlist in hitlists.items():
     max_pid = max(map(float, (h[1] for h in hitlist)))
     max_pid_diff = 5.00
     filtered = [h for h in hitlist if float(h[1]) >= max_pid-max_pid_diff]
